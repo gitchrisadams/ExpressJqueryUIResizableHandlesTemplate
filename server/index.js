f5325f94746f0a5ea69a8a08e2b2705109947604
gitchrisadams <chrismichaeladams@gmail.com>
2018-12-13 21:57:18 -0500

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.set('view engine', 'hbs');
+
+// Register Handlebars view engine
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
+// Use Handlebars view engine
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+// Pass in a test variable to be used in template:
+app.get('/', (req, res) => {
+  res.render('index', {"myvar": "myvar1"});
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
